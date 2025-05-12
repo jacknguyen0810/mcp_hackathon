3ed2f421079b71081d2217c38aa1f51037553223
AndreaSainz <as3628@cam.ac.uk>
2025-05-12 15:15:39 +0100

    last

---
+++ src/wikipedia.py
@@ -0,0 +1,12 @@
+import asyncio
+from wikipedia_note_agent import WikipediaNoteAgent
+import nest_asyncio
+import os
+
+nest_asyncio.apply()
+
+study_context = "Diffusion models are a type of machine learning models"
+api_key = os.getenv("OPENAI_API_KEY")
+
+agent = WikipediaNoteAgent(study_context=study_context, openai_api_key=api_key)
+asyncio.run(agent.run())
